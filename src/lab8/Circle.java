10fb85fb86583e43bed27ee6078f45b26fdcdc6f
williamd4112 <williamd4112@hotmail.com>
2014-12-02 16:26:53 +0800

    Main Class and some fix

--- src/lab8/Circle.java
+++ src/lab8/Circle.java
@@ -1,13 +1,18 @@
+package lab8;
 
 public class Circle {
 	
 	static final double PI = 3.141592653589793;
 	
-	public static void main(String[] args){
-		
-		double radius = 3 , area;
-		
-		area = PI * radius * radius;
-		System.out.println("Circle area is:" + area);
+	private double mRadius;
+	
+	public Circle(double radius)
+	{
+		mRadius = radius;
+	}
+	
+	public double getArea()
+	{
+		return mRadius * mRadius * PI;
 	}
 }
---
+++ src/lab8/Main.java
@@ -0,0 +1,13 @@
+package lab8;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c = new Circle(10);
+		Square s = new Square(10);
+		System.out.println(c.getArea());
+		System.out.println(s.getArea());
+	}
+
+}
